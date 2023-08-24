7577c5b9ea91abb5c0360b98be99417d094a09b7
Ben Hur <bhur777@gmail.com>
2023-08-24 04:28:01 -0400

    Get billboard data and display

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,16 @@
+import getBillboards from "@/actions/get-billboards";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+	const billboard = await getBillboards("948d7ad8-1f47-43b0-af2a-a018e5ced298");
+
 	return (
 		<Container>
 			<div className="space-y-10 pb-10">
-				<Billboard />
+				<Billboard data={billboard} />
 			</div>
 		</Container>
 	);
