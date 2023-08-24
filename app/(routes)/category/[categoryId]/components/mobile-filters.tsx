de1947d27fbc769312240813e42a3229e67460cd
Ben Hur <bhur777@gmail.com>
2023-08-24 08:06:18 -0400

    Create states to open filter on mobile

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,7 +1,29 @@
 "use client";
 
-const MobileFilters = () => {
-	return <div>Mobile filters</div>;
+import Button from "@/components/ui/button";
+import { Color, Size } from "@/types";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+
+interface MobileFitlersProps {
+	sizes: Size[];
+	colors: Color[];
+}
+
+const MobileFilters: React.FC<MobileFitlersProps> = ({ sizes, colors }) => {
+	const [open, setOpen] = useState(false);
+
+	const onOpen = () => setOpen(true);
+	const onClose = () => setOpen(false);
+
+	return (
+		<div>
+			<Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
+				Filters
+				<Plus size={20} />
+			</Button>
+		</div>
+	);
 };
 
 export default MobileFilters;
