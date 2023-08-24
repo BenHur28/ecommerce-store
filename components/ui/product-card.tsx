1e902d91a4087427d9bb6badd6c3a92db3337025
Ben Hur <bhur777@gmail.com>
2023-08-24 04:45:23 -0400

    create card for each product

---
+++ components/ui/product-card.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Product } from "@/types";
+
+interface ProductCardProps {
+	data: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+	return <div>Product Card</div>;
+};
+
+export default ProductCard;
