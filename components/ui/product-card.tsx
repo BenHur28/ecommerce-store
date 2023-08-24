17675c4fdc62f821f5b660523663d6ba9d9f850a
Ben Hur <bhur777@gmail.com>
2023-08-24 05:08:08 -0400

    change iconbutton classes

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -15,7 +15,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 		<div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
 			<div className="aspect-square rounded-xl bg-gray-100 relative">
 				<Image
-					src={data?.images?.[0].url}
+					src={data?.images?.[0]?.url}
 					fill
 					alt="image"
 					className="aspect-square object-cover rounded-md"
